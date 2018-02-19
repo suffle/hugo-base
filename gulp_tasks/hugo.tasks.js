1a7fb3dc74553d2e6a039481e360bb3e174e62a3
suffle <sebastian.flor@t-online.de>
2018-02-19 23:05:00 +0100

    Task: add basic build job

--- gulp_tasks/hugo.tasks.js
+++ gulp_tasks/hugo.tasks.js
@@ -1,5 +1,16 @@
+import gulp from "gulp";
 import { spawn } from "child_process";
 import hugo from "hugo-bin";
+import htmlmin from "gulp-htmlmin";
+
+import { minifierConf } from "../config/html.conf";
+
+export const minifyHtml = (src, dest) => {
+  return gulp
+    .src(src)
+    .pipe(htmlmin(minifierConf))
+    .pipe(gulp.dest(dest));
+};
 
 export const buildSite = (src, dest, cb, env = "development") => {
   const hugoArgsGeneral = ["-d", dest, "-s", src, "-v"];
--- src/site/content/posts/my-first-post.md
+++ src/site/content/posts/my-first-post.md
@@ -1,14 +1,7 @@
 ---
 title: "My First Post"
 date: 2017-12-09T00:01:53+01:00
-draft: false
+draft: true
 ---
-{{% figure src="/media/spf13.jpg" title="Steve Francia" %}}
-{{< gallery class="content-gallery" >}}
-{{< image src="/images/one.jpg" >}}
-{{< /gallery >}}
-{{< row margin="margin-big" >}}
-{{<column desktop-width="4">}}
-<p>Test</p>
-{{</column>}}
-{{< /row >}}
+
+{{< image src="/img/WinterSuite_2014.jpg" >}}
